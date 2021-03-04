a316c3e8ab3210b921d70b5e6bfb8416d4d1a72e
Oleh Shablii <oshabl@softserveinc.com>
2021-03-04 17:10:37 +0200

    forgot to save files :(

--- src/modules/news/news.controller.ts
+++ src/modules/news/news.controller.ts
@@ -1,17 +1,33 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Body, Get, Post, Patch, Delete,  Param } from '@nestjs/common';
 import News from './news.service';
+import INews from './news.interface';
 
 @Controller('news')
 export class NewsController {
     constructor(private readonly newsService: News) {}
 
     @Get()
-    getHello(): string {
+    getAllNews(){
       return this.newsService.getAll();
     }
 
-    @Get(':id'){
-
+    @Get(':id')    
+    getNewsById(@Param('id') id: string) {
+        return this.newsService.getById(Number(id))
     }
+
+    @Post()
+    async createPost(@Body() post: INews) {
+        return this.newsService.create(post);
+    }
+
+    @Patch(':id')
+    async updateNews(@Param('id') id: string, @Body() body: any) {
+        return this.newsService.update(Number(id), body);
+    }
+
+    @Delete(':id')
+    async deleteNews(@Param('id') id: string) {
+        return this.newsService.delete(Number(id));
     }
 }
--- src/modules/news/news.service.ts
+++ src/modules/news/news.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import INews from './news.interface';
 
-
-const NEWS = [
+//let - simply because its not important right now
+let mockNews = [
     {
         "id": 1,
         "title": "News1",
@@ -11,7 +11,7 @@ const NEWS = [
     {
         "id": 2,
         "title": "News2",
-        "description": "bla bla bla bla" 
+        "description": "bla bla bla bla bla" 
     }, 
 ]
 
@@ -19,10 +19,30 @@ const NEWS = [
 export default class News {
 
     getAll():Array<INews>{
-        return NEWS
+        return mockNews
     }
 
-    getById(_id):INews {
-        return NEWS.find( ({id}) => id === _id)
+    getById(_id:number):INews {
+        return mockNews.find( ({id}) => id === _id)
+    }
+
+    create(body:INews):string {
+        mockNews.push(body);
+        return 'Created'
+    }
+
+    update(_id:number, body:any):INews {
+        //Влом придумывать интерфейс сейчас - хоццо суть постичь
+        return mockNews.reduce<any>( (acc, cur) => {
+            if (cur.id === _id) {
+                acc = {...cur, ...body}
+            }
+            return acc
+        }, {})
+    }
+
+    delete (_id: number):string{
+        mockNews = mockNews.filter( ({id}) => id !==_id );
+        return 'deleted'
     }
 }
